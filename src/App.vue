46e619b6dcdf7a1a44b5b91ca5007c577df7a6a9
Andrew Ganzha <69760696+AndrewGanzha@users.noreply.github.com>
2023-12-07 16:15:39 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from "./components/Header/Header.vue";
+import Form from "./components/Form/Form.vue";
+</script>
+
+<template>
+  <Header />
+  <Form />
+</template>
+
+<style scoped></style>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SeriaSelect from "./SeriaSelect.vue";
+</script>
+
+<template>
+  <div class="container">
+    <SeriaSelect />
+  </div>
+</template>
+
+<style scoped>
+* {
+  padding-top: 20px;
+}
+</style>
