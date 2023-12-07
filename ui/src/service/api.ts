59341632c770c6551dfcec338ce02d4ae7dd5645
AndrewGanzha <ganzha-2002@mail.com>
2023-12-07 18:37:39 +0300

    Add files

--- ui/src/service/api.ts
+++ ui/src/service/api.ts
@@ -1,5 +1,15 @@
 import { defineStore } from "pinia";
+import {Reducer} from "./types.ts";
+import axios from "axios";
 
 export const conductorStore = defineStore("reductors", {
   state: () => ({}),
 });
+
+export function getReducers(): Reducer[] {
+  axios.get('http://localhost:8080/api/v1/reducerSeries').then(() => {
+    console.log('Success!')
+  })
+
+  return [];
+}
---
+++ ui/src/service/types.ts
@@ -0,0 +1,6 @@
+export interface Reducer {
+    id: number,
+    seriesName: string,
+    imgPath: string,
+    nameForPrint: string
+}
